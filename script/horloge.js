b56df844a5a9309df99beda797304dc518dfeddd
Martin Valet <martin.valet@etu.unilim.fr>
2025-02-12 16:22:47 +0100

    Ajout de la date
    Ajout de la date au forma [Jour de la Semaine] [Jour] [Mois] - [Heure] : [Minutes] : [Secondes]

---
+++ script/horloge.js
@@ -0,0 +1,30 @@
+function Horloge() {
+    const heureActuelle = new Date();
+    let heures = heureActuelle.getHours().toString().padStart(2, '0');
+    let minutes = heureActuelle.getMinutes().toString().padStart(2, '0');
+    let secondes = heureActuelle.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById("heure").textContent= `${heures} : ${minutes} : ${secondes}`;
+}
+function DateDuJour() {
+    const date = new Date();
+    let afficheJour = "";
+    let afficheMois = "";
+    const jourDeLaSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    const moisEnString = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
+
+    let jour = date.getDate();
+    let mois = date.getMonth() + 1;
+    let annee = date.getFullYear();
+
+    afficheJour = jourDeLaSemaine[date.getDay()];
+    afficheMois = moisEnString[mois];
+
+    const dateFormattee = `${afficheJour} ${jour} ${afficheMois}`;
+    document.getElementById("dateHorloge").textContent = dateFormattee;
+
+}
+DateDuJour();
+Horloge();
+setInterval(Horloge, 1000);
+setInterval(DateDuJour, 36000000);
