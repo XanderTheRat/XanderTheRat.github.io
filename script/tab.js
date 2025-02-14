3aa0731d4b18ed17420ae6dd77ebb47d7654f929
Martin Valet <martin.valet@etu.unilim.fr>
2025-02-14 11:31:52 +0100

    C140225-3
    Réparation des onglets principaux. Tout marche.

--- script/tab.js
+++ script/tab.js
@@ -1,18 +1,20 @@
-const tabs = document.querySelectorAll('.tabs');
+const tabs = document.querySelectorAll('.tab');
 const tabContents = document.querySelectorAll('.tab-content');
 const sousTabs = document.querySelectorAll('.sous-tabs');
 const sousTabContents = document.querySelectorAll('.sous-tab-content');
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
+        // Remove active class from all tabs and contents
         tabs.forEach(t => t.classList.remove('active'));
         tabContents.forEach(content => content.classList.remove('active'));
 
+        // Add active class to the clicked tab and corresponding content
         tab.classList.add('active');
         document.getElementById(tab.dataset.tab).classList.add('active');
     });
 });
-
+/*
 sousTabs.forEach(sousTab => {
     sousTab.addEventListener('click', () => {
         sousTabs.forEach(t => t.classList.remove('sousTabActive'));
@@ -22,3 +24,4 @@ sousTabs.forEach(sousTab => {
         document.getElementById(sousTab.dataset.sousTab).classList.add('sousTabActive');
     });
 });
+*/
