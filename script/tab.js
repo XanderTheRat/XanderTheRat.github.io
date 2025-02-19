94ad274e7cf4a0c17622dcfcc58ba44d1581d964
Martin Valet <martin.valet@etu.unilim.fr>
2025-02-19 15:32:16 +0100

    C190225-2
    Pré-build des sous-tab

--- script/tab.js
+++ script/tab.js
@@ -1,26 +1,27 @@
 const tabs = document.querySelectorAll('.tab');
 const tabContents = document.querySelectorAll('.tab-content');
-const sousTabs = document.querySelectorAll('.sous-tabs');
-const sousTabContents = document.querySelectorAll('.sous-tab-content');
+const sousTabs = document.querySelectorAll('.sousTab');
+const sousTabContents = document.querySelectorAll('.sousTab-content');
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-        // Remove active class from all tabs and contents
         tabs.forEach(t => t.classList.remove('active'));
         tabContents.forEach(content => content.classList.remove('active'));
-
-        // Add active class to the clicked tab and corresponding content
         tab.classList.add('active');
         document.getElementById(tab.dataset.tab).classList.add('active');
     });
 });
 
 sousTabs.forEach(sousTab => {
-    sousTab.addEventListener('click', () => {
-        sousTabs.forEach(t => t.classList.remove('sousTabActive'));
-        sousTabContents.forEach(content => content.classList.remove('sousTabActive'));
+    sousTab.addEventListener('click', (event) => {
+        const isActive = sousTab.classList.contains('sousTabActive');
 
-        sousTab.classList.add('sousTabActive');
-        document.getElementById(sousTab.dataset.sousTab).classList.add('sousTabActive');
+        if (isActive) {
+            sousTab.classList.remove('sousTabActive');
+            event.target.classList.remove('sousTabActive');
+        } else {
+            sousTab.classList.add('sousTabActive');
+            document.getElementById(event.target.id).classList.add('sousTabActive');
+        }
     });
 });
