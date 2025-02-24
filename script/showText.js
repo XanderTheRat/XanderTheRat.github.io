bc9bb530c7bce40ffa403547a905f1729afe1e29
Martin Valet <martin.valet@etu.unilim.fr>
2025-02-24 20:56:15 +0100

    C240215-4
    Fix some problems in the new page
    Add the possibility to change the size of the text by pressing "insert" and a number. Pressing the "enter" button apply the size

---
+++ script/showText.js
@@ -0,0 +1,70 @@
+let upperCase = false;
+let letter = "";
+let boolNumber = false
+let sizeOfText = 300;
+const exeption = ["Alt", "Control", "Home", "Shift", "End","PageDown","PageUp","F10","F9","F8","F7","F6","F5", "F4", "F3", "F2", "F1"]
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("keydown", function(event){
+
+        if (boolNumber) {
+            if (event.key <=9 || event.key >= 0) {
+                sizeOfText += event.key;
+            }
+            else if (event.key === "Enter") {
+                boolNumber = false;
+                // On change la taille du texte saisie
+                document.documentElement.style.setProperty('--sizeOfText', sizeOfText + "px");
+                boolNumber = false;
+            }
+            else if (event.key === "Backspace"){
+                letter = letter.substring(0, letter.length - 1);
+                document.getElementById("mainDiv").textContent = letter;
+            }
+            else if (event.key === "Delete") {
+                letter = "";
+                document.getElementById("mainDiv").textContent = letter;
+            }
+        }
+        // Si ce n'est pas un chiffre à rentrer
+        else {
+            if (event.key === "CapsLock") {
+                if (upperCase) {
+                    upperCase = false
+                    letter += event.key.toUpperCase();
+                }else{
+                    upperCase = true;
+                    letter += event.key.toLowerCase();
+                }
+                letter = letter.substring(0, letter.length - 8);
+            }
+            else if (event.key === "Backspace"){
+                letter = letter.substring(0, letter.length - 1);
+                document.getElementById("mainDiv").textContent = letter;
+            }
+            else if (event.key === "Delete") {
+                letter = "";
+                document.getElementById("mainDiv").textContent = letter;
+            }
+            else if (event.key === "Enter") {
+                letter += "";
+            }
+            else if (event.key === "Insert") {
+                sizeOfText = 0;
+                boolNumber = true;
+            }
+            else if (exeption.includes(event.key)) {
+                letter += "";
+            }
+            else {
+                letter += event.key;
+                document.getElementById("mainDiv").textContent = letter;
+            }
+        }
+        if (letter.length > 255) {
+                letter[1] = event.key;
+        }
+
+    })
+});
