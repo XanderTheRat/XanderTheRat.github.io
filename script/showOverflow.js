21c2594abad9cae8275ae256b15c4821fb30a041
Martin Valet <saussic0canard@gmail.com>
2025-03-12 15:17:31 +0100

    C120325-2
    Change the command for hiding and showing the overflow : Add a system to hide or show a specific overflow.
    
    Also add the french

--- script/showOverflow.js
+++ script/showOverflow.js
@@ -2,17 +2,39 @@ let caracters = "";
 
 document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener("keydown", function(event){
-        if (caracters.length > 50) {
+        if (caracters.length > 200) {
             caracters.substring(caracters.length - 10, caracters.length);
         }
         caracters += event.key;
         if (caracters.toLowerCase().includes("shift") && caracters.toLowerCase().includes("alt")) {
-            document.documentElement.style.setProperty("--overflow", "hidden");
-            caracters = "";
+            if (caracters.toLowerCase().includes("arrowleft") || caracters.toLowerCase().includes("arrowright")) {
+                document.documentElement.style.setProperty("--overflowX", "hidden");
+                caracters = "";
+            }
+            else if (caracters.toLowerCase().includes("arrowup") || caracters.toLowerCase().includes("arrowdown")) {
+                document.documentElement.style.setProperty("--overflowY", "hidden");
+                caracters = "";
+            }
+            else {
+                document.documentElement.style.setProperty("--overflowX", "hidden");
+                document.documentElement.style.setProperty("--overflowY", "hidden");
+                caracters = "";
+            }
         }
         else if (caracters.toLowerCase().includes("alt") && caracters.toLowerCase().includes("control")) {
-            document.documentElement.style.setProperty("--overflow", "auto");
-            caracters = "";
+            if (caracters.toLowerCase().includes("arrowleft") || caracters.toLowerCase().includes("arrowright")) {
+                document.documentElement.style.setProperty("--overflowX", "auto");
+                caracters = "";
+            }
+            else if (caracters.toLowerCase().includes("arrowup") || caracters.toLowerCase().includes("arrowdown")) {
+                document.documentElement.style.setProperty("--overflowY", "auto");
+                caracters = "";
+            }
+            else {
+                document.documentElement.style.setProperty("--overflowX", "auto");
+                document.documentElement.style.setProperty("--overflowY", "auto");
+                caracters = "";
+            }
         }
     });
 });
