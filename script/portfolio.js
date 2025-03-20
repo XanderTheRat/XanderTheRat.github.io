0464fe126a132cb155293cc51904e0a4a4501de5
Martin Valet <saussic0canard@gmail.com>
2025-03-20 17:17:00 +0100

    C200325-1
    Add SQL on the page

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,6 +1,11 @@
 let lienActif;
 const lienDemineur = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/Python/Demineur_upgrade.py";
 const lienChronometre = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/Python/Chronometre_amélioré.py";
+const lienBDD1ACreation = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/BDD.sql";
+const lienBBD1AExos = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/Exos.sql";
+const lienInsertionVGSales = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/Requetes.sql";
+const lienBDDRemplirVGSales = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/remplissage.sql";
+const lienBDD1ACreationTablesVGSales = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/tables.sql";
 
 function setLienDemineur() {
     lienActif = lienDemineur;
@@ -10,6 +15,31 @@ function setLienChronometre() {
     lienActif = lienChronometre;
     afficherCode();
 }
+
+
+function setLienBDD1ACreation() {
+    lienActif = lienBDD1ACreation;
+    afficherCode();
+}
+function setLienBDD1AExos() {
+    lienActif = lienBBD1AExos;
+    afficherCode();
+}
+function setLienInsertionVGSales() {
+    lienActif = lienInsertionVGSales;
+    afficherCode();
+}
+function setLienBDDRemplirVGSales() {
+    lienActif = lienBDDRemplirVGSales;
+    afficherCode();
+}
+function setLienBDD1ACreationTablesVGSales() {
+    lienActif = lienBDD1ACreationTablesVGSales;
+    afficherCode();
+}
+
+
+
 
 document.addEventListener("keydown", function(event){
     if (event.key.toLowerCase() === "r") {
