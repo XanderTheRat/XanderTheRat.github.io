7ba4745b8027ce1a4db02cfb713e391a2dd7d84e
Martin Valet <saussic0canard@gmail.com>
2025-03-22 19:16:50 +0100

    C220325
    Add the web on the page.
    Add image coresponding on the langage, and redimensiones them on 225*225.

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,12 +1,39 @@
 let lienActif;
+// Liens Python
 const lienDemineur = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/Python/Demineur_upgrade.py";
 const lienChronometre = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/Python/Chronometre_amélioré.py";
+//Lien HTML/CSS/JS
+const lienHalloweenIndex = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/web/Halloween/index.html";
+const lienHalloweenCSS = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/web/Halloween/style/style.css";
+const lienHalloweenJS = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/web/Halloween/script/script.js";
+
+const lienXanderTheRatIndex = "https://raw.githubusercontent.com/XanderTheRat/XanderTheRat.github.io/main/index.html";
+const lienXanderTheRatPortfolio = "https://raw.githubusercontent.com/XanderTheRat/XanderTheRat.github.io/main/portfolio.html";
+const lienXanderTheRatAfficherTexte = "https://raw.githubusercontent.com/XanderTheRat/XanderTheRat.github.io/main/afficherText.html";
+const lienXanderTheRatResetCSS = "https://raw.githubusercontent.com/XanderTheRat/XanderTheRat.github.io/main/style/cssReset.css";
+const lienXanderTheRatMainCSS = "https://raw.githubusercontent.com/XanderTheRat/XanderTheRat.github.io/main/style/main.css";
+const lienXanderTheRatHorloge = "https://raw.githubusercontent.com/XanderTheRat/XanderTheRat.github.io/main/script/horloge.js";
+const lienXanderTheRatPortfolioJS = "https://raw.githubusercontent.com/XanderTheRat/XanderTheRat.github.io/main/script/portfolio.js";
+const lienXanderTheRatShowText = "https://raw.githubusercontent.com/XanderTheRat/XanderTheRat.github.io/main/script/showText.js";
+//Lien Bash
+
+// Liens SQL : VGSales
 const lienBDD1ACreation = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/BDD.sql";
 const lienBBD1AExos = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/Exos.sql";
 const lienInsertionVGSales = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/Requetes.sql";
 const lienBDDRemplirVGSales = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/remplissage.sql";
 const lienBDD1ACreationTablesVGSales = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/SQL/tables.sql";
+//Lien Java : SAÉ Java FX
 
+//Liens C
+const lienTP1C = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/C/R2.04/TP1/TP1.c";
+const lienTP2C = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/C/R2.04/TP2/tp2.c";
+const lienTP3C = "https://raw.githubusercontent.com/XanderTheRat/BUT1/main/C/R2.04/TP3/TP3.c";
+//Liens PostGre SQL
+
+// Autres variables
+
+// Fonctions Python
 function setLienDemineur() {
     lienActif = lienDemineur;
     afficherCode();
@@ -16,7 +43,53 @@ function setLienChronometre() {
     afficherCode();
 }
 
+// Fonction HTML/CSS/JS
+function setLienIndexHalloween() {
+    lienActif = lienHalloweenIndex;
+    afficherCode();
+}
+function setLienCSSHalloween() {
+    lienActif = lienHalloweenCSS;
+    afficherCode();
+}
+function setLienJSHalloween() {
+    lienActif = lienHalloweenJS;
+    afficherCode();
+}
 
+function setLienIndexXanderTheRat() {
+    lienActif = lienXanderTheRatIndex;
+    afficherCode();
+}
+function setLienPortfolioXanderTheRat() {
+    lienActif = lienXanderTheRatPortfolio;
+    afficherCode();
+}
+function setLienAfficherTexte() {
+    lienActif = lienXanderTheRatAfficherTexte;
+    afficherCode();
+}
+function setLienResetCSS() {
+    lienActif = lienXanderTheRatResetCSS;
+    afficherCode();
+}
+function setLienMainCSS() {
+    lienActif = lienXanderTheRatMainCSS;
+    afficherCode();
+}
+function setLienHorloge() {
+    lienActif = lienXanderTheRatHorloge;
+    afficherCode();
+}
+function setLienPortfolioJS() {
+    lienActif = lienXanderTheRatPortfolioJS;
+    afficherCode();
+}
+function setLienShowText() {
+    lienActif = lienXanderTheRatShowText;
+    afficherCode();
+}
+//Fonctions SQL
 function setLienBDD1ACreation() {
     lienActif = lienBDD1ACreation;
     afficherCode();
@@ -38,6 +111,19 @@ function setLienBDD1ACreationTablesVGSales() {
     afficherCode();
 }
 
+//Fonctions C
+function setLienTP1C() {
+    lienActif = lienTP1C;
+    afficherCode();
+}
+function setLienTP2C() {
+    lienActif = lienTP2C;
+    afficherCode();
+}
+function setLienTP3C() {
+    lienActif = lienTP3C;
+    afficherCode();
+}
 
 
 
@@ -67,4 +153,14 @@ function escapeHtml(text) {
     return text.replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;");
+}
+
+function collapsible(id) {
+    if (document.getElementById(id).style.display === "none") {
+        document.getElementById(id).style.display = "block";
+        document.getElementById(id + "Collapsible").classList.add("collapsibleMinus")
+    }else{
+        document.getElementById(id).style.display = "none";
+        document.getElementById(id + "Collapsible").classList.remove("collapsibleMinus")
+    }
 }
