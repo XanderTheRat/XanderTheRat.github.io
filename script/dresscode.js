a90386542d7df7b395298a2c2be707a03534d771
Martin Valet <martin.valet@etu.unilim.fr>
2025-03-24 00:31:29 +0100

    FIM 6

--- script/dresscode.js
+++ script/dresscode.js
@@ -1,28 +1,56 @@
-const tabIDs = ["tenueHommeStandardFace", "tenueHommeStandardDos", "tenueFemmeStandardFace", "tenueFemmeStandardDos"]
-let compteurID = 0;
+const tabTenueStandart = ["tenueHommeStandardFace", "tenueHommeStandardDos", "tenueFemmeStandardFace", "tenueFemmeStandardDos"];
+const tabTenuesCommandement = ["tenuesHommeCommandementFace", "tenueHommeCommandementDos", "tenueFemmeCommandementFace", "tenueFemmeCommandementDos"];
+let compteurTenuesStandart = 0;
+let compteurTenuesCommandement = 0;
 
-function setActifPlus(){
-    document.getElementById(tabIDs[compteurID]).classList.remove('active');
-    document.getElementById(tabIDs[compteurID]).classList.add('inactif');
-    if ((compteurID + 1) === 4) {
-        compteurID = 0;
+function setActifPlusTenuesStandards(){
+    document.getElementById(tabTenueStandart[compteurTenuesStandart]).classList.remove('active');
+    document.getElementById(tabTenueStandart[compteurTenuesStandart]).classList.add('inactif');
+    if ((compteurTenuesStandart + 1) === 4) {
+        compteurTenuesStandart = 0;
     }
     else {
-        compteurID++;
+        compteurTenuesStandart++;
     }
-    document.getElementById(tabIDs[compteurID]).classList.add('active');
-    document.getElementById(tabIDs[compteurID]).classList.remove('inactif');
+    document.getElementById(tabTenueStandart[compteurTenuesStandart]).classList.add('active');
+    document.getElementById(tabTenueStandart[compteurTenuesStandart]).classList.remove('inactif');
 }
 
-function setActifMoins() {
-    document.getElementById(tabIDs[compteurID]).classList.remove('active');
-    document.getElementById(tabIDs[compteurID]).classList.add('inactif');
-    if ((compteurID) === 0) {
-        compteurID = 3;
+function setActifMoinsTenuesStandards() {
+    document.getElementById(tabTenueStandart[compteurTenuesStandart]).classList.remove('active');
+    document.getElementById(tabTenueStandart[compteurTenuesStandart]).classList.add('inactif');
+    if ((compteurTenuesStandart) === 0) {
+        compteurTenuesStandart = 3;
     }
     else {
-        compteurID--;
+        compteurTenuesStandart--;
     }
-    document.getElementById(tabIDs[compteurID]).classList.add('active');
-    document.getElementById(tabIDs[compteurID]).classList.remove('inactif');
+    document.getElementById(tabTenueStandart[compteurTenuesStandart]).classList.add('active');
+    document.getElementById(tabTenueStandart[compteurTenuesStandart]).classList.remove('inactif');
+}
+
+function setActifPlusTenuesCommandement(){
+    document.getElementById(tabTenuesCommandement[compteurTenuesCommandement]).classList.remove('active');
+    document.getElementById(tabTenuesCommandement[compteurTenuesCommandement]).classList.add('inactif');
+    if ((compteurTenuesCommandement + 1) === 4) {
+        compteurTenuesCommandement = 0;
+    }
+    else {
+        compteurTenuesCommandement++;
+    }
+    document.getElementById(tabTenuesCommandement[compteurTenuesCommandement]).classList.add('active');
+    document.getElementById(tabTenuesCommandement[compteurTenuesCommandement]).classList.remove('inactif');
+}
+
+function setActifMoinsTenuesCommandement() {
+    document.getElementById(tabTenuesCommandement[compteurTenuesCommandement]).classList.remove('active');
+    document.getElementById(tabTenuesCommandement[compteurTenuesCommandement]).classList.add('inactif');
+    if ((compteurTenuesCommandement) === 0) {
+        compteurTenuesCommandement = 3;
+    }
+    else {
+        compteurTenuesCommandement--;
+    }
+    document.getElementById(tabTenuesCommandement[compteurTenuesCommandement]).classList.add('active');
+    document.getElementById(tabTenuesCommandement[compteurTenuesCommandement]).classList.remove('inactif');
 }
